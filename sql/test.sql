f09f79fe278f1766c0b7384b06fdae4e48b15a30
Ashish Mishra <60169684+1ashishji@users.noreply.github.com>
2021-02-21 17:21:08 +0530

    Add files via upload

--- sql/test.sql
+++ sql/test.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 12, 2018 at 10:05 AM
--- Server version: 5.7.24-0ubuntu0.18.04.1
--- PHP Version: 7.2.10-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Feb 21, 2021 at 12:50 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,6 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `child`
+--
+
+CREATE TABLE `child` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `state` int(100) NOT NULL,
+  `district` int(100) NOT NULL,
+  `sex` varchar(11) NOT NULL,
+  `dob` date NOT NULL,
+  `father` varchar(50) NOT NULL,
+  `mother` varchar(50) NOT NULL,
+  `img` varchar(1121) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `child`
+--
+
+INSERT INTO `child` (`id`, `name`, `state`, `district`, `sex`, `dob`, `father`, `mother`, `img`, `created_at`, `updated_at`) VALUES
+(1, 'Prakash Kumar', 1, 1, 'Male', '2001-01-02', 'Ram Kapoor', 'Ram Sushil', 'home.png', '2021-02-18 21:46:28', '2021-02-18 21:46:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `district`
+--
+
+CREATE TABLE `district` (
+  `id` int(11) NOT NULL,
+  `state_id` int(100) NOT NULL,
+  `district` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `district`
+--
+
+INSERT INTO `district` (`id`, `state_id`, `district`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Lucknow', '2021-02-18 00:09:08', '2021-02-18 00:09:08'),
+(2, 1, 'Kanpur', '2021-02-19 21:30:13', '2021-02-19 21:30:13'),
+(3, 1, 'testDistrictName', '2021-02-21 03:25:59', '2021-02-21 03:25:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_salary`
+--
+
+CREATE TABLE `employee_salary` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `salary` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee_salary`
+--
+
+INSERT INTO `employee_salary` (`id`, `name`, `salary`) VALUES
+(1, 'ash', 122),
+(2, 'ds', 123);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `news`
 --
 
@@ -31,7 +102,7 @@ CREATE TABLE `news` (
   `title` varchar(128) NOT NULL,
   `slug` varchar(128) NOT NULL,
   `text` text NOT NULL,
-  `user_id` int(11) NOT NULL DEFAULT '0'
+  `user_id` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -39,10 +110,35 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `title`, `slug`, `text`, `user_id`) VALUES
-(2, 'What is Lorem Ipsum?', 'what-is-lorem-ipsum', 'Lorem Ipsum is simply dummy text.x', 1),
 (3, 'My test', 'my-test', 'hello there', 2),
 (4, 'What is Lorem Ipsum?', 'what-is-lorem-ipsum', 'adsadsd', 1),
 (5, 'What is Lorem Ipsum?1', 'what-is-lorem-ipsum1', 'What is Lorem Ipsum?1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `id` int(121) NOT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`id`, `state`, `created_at`, `updated_at`) VALUES
+(1, 'Utter Pradesh', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Bihar', '2021-02-18 00:01:48', '2021-02-18 00:01:48'),
+(3, 'UttraKhand', '2021-02-18 00:02:18', '2021-02-18 00:02:18'),
+(4, 'Goa', '2021-02-19 20:16:57', '2021-02-19 20:16:57'),
+(5, 'Punjab', '2021-02-19 20:19:01', '2021-02-19 20:19:01'),
+(6, 'testStateName166vcxv', '2021-02-21 03:04:52', '2021-02-21 03:04:52'),
+(7, 'testStateName166vcxv', '2021-02-21 03:17:21', '2021-02-21 03:17:21');
 
 -- --------------------------------------------------------
 
@@ -56,6 +152,8 @@ CREATE TABLE `user` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `token` varchar(1121) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
   `updated_at` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -63,12 +161,33 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `updated_at`) VALUES
-(1, 'Gowthaman', 'Durai', 'gowthaman.nkl1@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2018-11-08 17:22:58');
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `token`, `last_login`, `updated_at`) VALUES
+(1, 'Gowthaman', 'Durai', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'NgNwMQOANg', '2021-02-20 21:52:11', '2018-11-08 17:22:58');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `child`
+--
+ALTER TABLE `child`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `district` (`district`),
+  ADD KEY `state` (`state`);
+
+--
+-- Indexes for table `district`
+--
+ALTER TABLE `district`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `state_id` (`state_id`);
+
+--
+-- Indexes for table `employee_salary`
+--
+ALTER TABLE `employee_salary`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `news`
@@ -76,6 +195,12 @@ INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `updated
 ALTER TABLE `news`
   ADD PRIMARY KEY (`id`),
   ADD KEY `slug` (`slug`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -89,15 +214,59 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `child`
+--
+ALTER TABLE `child`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `district`
+--
+ALTER TABLE `district`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `employee_salary`
+--
+ALTER TABLE `employee_salary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `news`
 --
 ALTER TABLE `news`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(121) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `child`
+--
+ALTER TABLE `child`
+  ADD CONSTRAINT `child_ibfk_1` FOREIGN KEY (`district`) REFERENCES `district` (`id`),
+  ADD CONSTRAINT `child_ibfk_2` FOREIGN KEY (`state`) REFERENCES `state` (`id`);
+
+--
+-- Constraints for table `district`
+--
+ALTER TABLE `district`
+  ADD CONSTRAINT `district_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `state` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
